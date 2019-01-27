11aa90106551299ab632b04e8d1d6ee1531c764f
Jorge Prados <jorge.prados@tailwindsoft.com>
2019-01-27 16:25:26 +0100

    Añadido proyecto WebApiAprendizaje

---
+++ ProyectoWebApiAprendizaje/Models/Model/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Model
+{
+    public class Cliente: BaseModel
+    {
+        public string Nombre { get; set; }
+    }
+}
---
+++ ProyectoWebApiAprendizaje/Respository/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get;set; }
+    }
+}
